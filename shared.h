b52d739c26b64e0d266ffaa47ded5f5be9cdf86f
handaimaoh <patlaborforce@gmail.com>
2013-11-07 12:43:55 -0600

    Move in function pointers. Use stdint types.

--- shared.h
+++ shared.h
@@ -30,43 +30,49 @@
 #define SSE_EMMS
 
 typedef struct {
-    __declspec(align(SSESIZE)) unsigned char noiselevel[SSESIZE];
-    unsigned char *blockproperties_addr;
-    unsigned threshold;
-    int pline;
-    int nline;
-    int motionblocks;
-    unsigned char *blockproperties;
-    int linewidth;
-    int hblocks;
-    int vblocks;
+    __declspec(align(SSESIZE)) uint8_t noiselevel[SSESIZE];
+    uint8_t *blockproperties_addr;
+    uint32_t threshold;
+    int32_t pline;
+    int32_t nline;
+    int32_t motionblocks;
+    uint8_t *blockproperties;
+    int32_t linewidth;
+    int32_t hblocks;
+    int32_t vblocks;
+    int32_t hblocksSSE2;
+    bool remainderSSE2;
+    int32_t linewidthSSE2;
+    uint32_t (__stdcall *blockcompare)(const uint8_t *p1, int32_t pitch1, const uint8_t *p2, int32_t pitch2);
+    void (__stdcall *blockcompareSSE2)(const uint8_t *p1, const uint8_t *p2, int32_t pitch);
 } MotionDetectionData;
 
 typedef struct {
-    int distblocks;
+    int32_t distblocks;
     uint32_t blocks;
     uint32_t tolerance;
     uint32_t *isum;
-    int dist;
-    int dist1;
-    int dist2;
-    int hinterior;
-    int vinterior;
-    int colinc;
-    int isumline;
-    int isuminc1;
-    int isuminc2;
+    int32_t dist;
+    int32_t dist1;
+    int32_t dist2;
+    int32_t hint32_terior;
+    int32_t vint32_terior;
+    int32_t colinc;
+    int32_t isumline;
+    int32_t isuminc1;
+    int32_t isuminc2;
+    void (__stdcall *processneighbours)(MotionDetectionDistData*);
     MotionDetectionData md;
 } MotionDetectionDistData;
 
 typedef struct {
-    int linewidthUV;
-    int chromaheight;
-    int chromaheightm;
-    int pthreshold;
-    int cthreshold;
-    int loops;
-    int restored_blocks;
+    int32_t linewidthUV;
+    int32_t chromaheight;
+    int32_t chromaheightm;
+    int32_t pthreshold;
+    int32_t cthreshold;
+    int32_t loops;
+    int32_t restored_blocks;
     MotionDetectionDistData mdd;
 } PostProcessingData;
 
@@ -80,7 +86,7 @@ void VS_CC SCSelectCreate(const VSMap *in, VSMap *out, void *userData, VSCore *c
 void VS_CC RestoreMotionBlocksCreate(const VSMap *in, VSMap *out, void *userData, VSCore *core, const VSAPI *vsapi);
 void VS_CC CreateDupBlocks(const VSMap *in, VSMap *out, void *userData, VSCore *core, const VSAPI *vsapi);
 
-unsigned gdiff(const unsigned char *sp1, int spitch1, const unsigned char *sp2, int spitch2, int hblocks, int incpitch, int height);
+uint32_t gdiff(const uint8_t *sp1, int32_t spitch1, const uint8_t *sp2, int32_t spitch2, int32_t hblocks, int32_t incpitch, int32_t height);
 void copyChroma(VSFrameRef *dest, const VSFrameRef *source, const VSVideoInfo *vi, const VSAPI *vsapi);
-int RemoveDirtProcessFrame(RemoveDirtData *rd, VSFrameRef *dest, const VSFrameRef *src, const VSFrameRef *previous, const VSFrameRef *next, int frame, const VSAPI *vsapi);
+int32_t RemoveDirtProcessFrame(RemoveDirtData *rd, VSFrameRef *dest, const VSFrameRef *src, const VSFrameRef *previous, const VSFrameRef *next, int32_t frame, const VSAPI *vsapi);
 void FillRemoveDirt(const VSMap *in, VSMap *out, const VSAPI *vsapi, RemoveDirtData *rd, const VSVideoInfo *vi);
