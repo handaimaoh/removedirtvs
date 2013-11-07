82ec14e3e46170f7b2a7631e84a71978fa2a8f61
handaimaoh <patlaborforce@gmail.com>
2013-11-07 12:43:10 -0600

    Remove unused code. Use stdint types.

--- DupBlocks.cpp
+++ DupBlocks.cpp
@@ -4,8 +4,8 @@ typedef struct {
     VSNodeRef *input;
     VSFrameRef *lf;
     const VSVideoInfo *vi;
-    int mthreshold;
-    int lfnr;
+    int32_t mthreshold;
+    int32_t lfnr;
     RemoveDirtData rd;
 } DupBlocksData;
 
@@ -17,7 +17,7 @@ static void VS_CC DupBlocksFree(void *instanceData, VSCore *core, const VSAPI *v
     free(d);
 }
 
-static const VSFrameRef *VS_CC DupBlocksGetFrame(int n, int activationReason, void **instanceData, void **frameData, VSFrameContext *frameCtx, VSCore *core, const VSAPI *vsapi)
+static const VSFrameRef *VS_CC DupBlocksGetFrame(int32_t n, int32_t activationReason, void **instanceData, void **frameData, VSFrameContext *frameCtx, VSCore *core, const VSAPI *vsapi)
 {
     DupBlocksData *d = (DupBlocksData *) *instanceData;
 
@@ -70,12 +70,12 @@ void VS_CC DupBlocksCreate(const VSMap *in, VSMap *out, void *userData, VSCore *
 
     d.lfnr = -2;
 
-    int err;
+    int32_t err;
     d.mthreshold = vsapi->propGetInt(in, "gmthreshold", 0, &err);
     if (err) {
         d.mthreshold = 80;
     }
-    d.mthreshold = (d.mthreshold * d.rd.hblocks * d.rd.vblocks) / 100;
+    d.mthreshold = (d.mthreshold * d.rd.pp.mdd.md.hblocks * d.rd.pp.mdd.md.vblocks) / 100;
 
     FillRemoveDirt(in, out, vsapi, &d.rd, vsapi->getVideoInfo(d.input));
 
@@ -84,44 +84,3 @@ void VS_CC DupBlocksCreate(const VSMap *in, VSMap *out, void *userData, VSCore *
 
     vsapi->createFilter(in, out, "DupBlocks", DupBlocksInit, DupBlocksGetFrame, DupBlocksFree, fmParallel, 0, data, core);
 }
-
-class	DupBlocks : public GenericVideoFilterCopy
-{
-protected:
-    RemoveDirtData rd;
-    int	mthreshold;
-
-    int		lfnr;
-    PVideoFrame	lf;
-
-    PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env)
-    {
-        PVideoFrame rf = child->GetFrame(n, env);
-        if( n - 1 != lfnr )
-        {
-            if( n == 0 ) return rf;
-            lf = GetWritableChildFrame(n - 1, env);
-        }
-
-        if( rd.show ) CopyChroma(lf, rf, env);
-
-        if( rd.ProcessFrame(lf, rf, lf, rf, n) > mthreshold ) return rf;
-        lfnr = n;
-        return CopyFrame(lf, env);
-    }
-
-    enum	creatargs { SRC, PLANAR, SHOW, DEBUG, GMTHRES, MTHRES, NOISE, NOISY, DIST, TOLERANCE, DMODE, PTHRES, CTHRES, GREY, REDUCEF};
-public:	
-    DupBlocks(AVSValue args, IScriptEnvironment *env)
-        : GenericVideoFilterCopy(args[SRC].AsClip(), args[GREY].AsBool(false)), lfnr(-2)
-        , rd(vi.width, vi.height, args[DIST].AsInt(DEFAULT_DIST), args[TOLERANCE].AsInt(DEFAULT_TOLERANCE), args[DMODE].AsInt(0)
-        , args[MTHRES].AsInt(100), args[NOISE].AsInt(0), args[NOISY].AsInt(-1), vi.IsYUY2()
-        , args[PTHRES].AsInt(DEFAULT_PTHRESHOLD), args[CTHRES].AsInt(DEFAULT_PTHRESHOLD)
-        , args[GREY].AsBool(false), args[SHOW].AsBool(false), args[DEBUG].AsBool(false), env)
-    {
-        mthreshold = (args[GMTHRES].AsInt(80) * rd.hblocks * rd.vblocks) / 100;
-        if( vi.IsRGB() || (vi.IsYV12() + args[PLANAR].AsBool(false) == 0) )
-            env->ThrowError("RemoveDirt: only YV12 and planar YUY2 clips are supported");
-        //child->SetCacheHints(CACHE_NOTHING, 0);
-    }
-};
