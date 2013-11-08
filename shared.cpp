aeb5b278dc1d5b54de966dee37a970462b9c1584
handaimaoh <patlaborforce@gmail.com>
2013-11-08 12:23:05 -0600

    "clense" -> "cleanse"

--- shared.cpp
+++ shared.cpp
@@ -122,13 +122,13 @@ VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegiste
                               "grey:int32_t:opt", DupBlocksCreate, 0, plugin);
     registerFunc("Cleanse", "input:clip"\
                             "grey:int:opt"\
-                            "reduceflicker:int:opt", ClenseCreate, 0);
+                            "reduceflicker:int:opt", CleanseCreate, 0);
     registerFunc("MCCleanse", "input:clip"\
                               "previous:clip"\
                               "next:clip"\
-                              "grey:int:opt", MCClenseCreate, 0);
+                              "grey:int:opt", MCCleanseCreate, 0);
     registerFunc("BackwardCleanse", "input:clip"\
-                                    "grey:int:opt", BackwardClenseCreate, 0);
+                                    "grey:int:opt", BackwardCleanseCreate, 0);
     registerFunc("ForwardCleanse", "input:clip"\
-                                   "grey:int:opt", ForwardClenseCreate, 0);
+                                   "grey:int:opt", ForwardCleanseCreate, 0);
 }
