f39d960b3aca0a1a0015fc6ebaf237d312507e6a
paradoxical <paradoxical@gmail.com>
2013-11-16 10:33:14 -0600

    Fix bug introduced by a find and replace

--- shared.cpp
+++ shared.cpp
@@ -99,25 +99,25 @@ VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegiste
                                         "neighbour:clip:opt;"\
                                         "neighbour2:clip:opt;"\
                                         "alternative:clip:opt;"\
-                                        "gmthreshold:int32_t:opt;"\
-                                        "mthreshold:int32_t:opt;"\
-                                        "noise:int32_t:opt;"\
-                                        "noisy:int32_t:opt;"\
-                                        "dist:int32_t:opt;"\
-                                        "tolerance:int32_t:opt;"\
-                                        "dmode:int32_t:opt;"\
-                                        "pthreshold:int32_t:opt;"\
-                                        "cthreshold:int32_t:opt;"\
-                                        "grey:int32_t:opt;", RestoreMotionBlocksCreate, 0, plugin);
+                                        "gmthreshold:int:opt;"\
+                                        "mthreshold:int:opt;"\
+                                        "noise:int:opt;"\
+                                        "noisy:int:opt;"\
+                                        "dist:int:opt;"\
+                                        "tolerance:int:opt;"\
+                                        "dmode:int:opt;"\
+                                        "pthreshold:int:opt;"\
+                                        "cthreshold:int:opt;"\
+                                        "grey:int:opt;", RestoreMotionBlocksCreate, 0, plugin);
     registerFunc("DupBlocks", "input:clip;"\
-                              "gmthreshold:int32_t:opt;"\
-                              "mthreshold:int32_t:opt;"\
-                              "noise:int32_t:opt;"\
-                              "noisy:int32_t:opt;"\
-                              "dist:int32_t:opt;"\
-                              "tolerance:int32_t:opt;"\
-                              "dmode:int32_t:opt;"\
-                              "pthreshold:int32_t:opt;"\
-                              "cthreshold:int32_t:opt;"\
-                              "grey:int32_t:opt;", DupBlocksCreate, 0, plugin);
+                              "gmthreshold:int:opt;"\
+                              "mthreshold:int:opt;"\
+                              "noise:int:opt;"\
+                              "noisy:int:opt;"\
+                              "dist:int:opt;"\
+                              "tolerance:int:opt;"\
+                              "dmode:int:opt;"\
+                              "pthreshold:int:opt;"\
+                              "cthreshold:int:opt;"\
+                              "grey:int:opt;", DupBlocksCreate, 0, plugin);
 }
