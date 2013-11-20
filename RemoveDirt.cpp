47fb652abe03aad183ac4efbfc525709cfeeb3e0
paradoxical <paradoxical@gmail.com>
2013-11-19 18:53:48 -0600

    Purging MMX. Converted horivertical_diff_yv12_chroma function

--- RemoveDirt.cpp
+++ RemoveDirt.cpp
@@ -906,39 +906,40 @@ static __forceinline int32_t horizontal_diff_chroma(const uint8_t *u, const uint
 
 static __forceinline int32_t vertical_diff_yv12_chroma(const uint8_t *u, const uint8_t *v, int32_t pitch, const uint8_t *noiselevel)
 {
-    __m64 mm7 = *((__m64*)noiselevel);
+    __m128i xmm7 = _mm_loadu_si128((__m128i*)noiselevel);
     int32_t pitchx2 = pitch + pitch;
     int32_t pitchx3 = pitchx2 + pitch;
 
-    __m64 mm0, mm1;
-    mm0 = _mm_insert_pi16(mm0, *((int32_t*)u), 0);
-    mm0 = _mm_insert_pi16(mm0, *((int32_t*)(u+pitchx2)), 1);
-    mm1 = _mm_insert_pi16(mm1, *((int32_t*)(u+pitch)), 0);
-    mm1 = _mm_insert_pi16(mm1, *((int32_t*)(u+pitchx3)), 1);
-    mm7 = _mm_cmpeq_pi8(mm7, mm7);
+    __m128i xmm0, xmm1;
+    xmm0 = _mm_insert_epi16(xmm0, *((int32_t*)u), 0);
+    xmm0 = _mm_insert_epi16(xmm0, *((int32_t*)(u+pitchx2)), 1);
+    xmm1 = _mm_insert_epi16(xmm1, *((int32_t*)(u+pitch)), 0);
+    xmm1 = _mm_insert_epi16(xmm1, *((int32_t*)(u+pitchx3)), 1);
 
-    mm0 = _mm_insert_pi16(mm0, *((int32_t*)v), 2);
-    mm0 = _mm_insert_pi16(mm0, *((int32_t*)(v+pitchx2)), 3);
-    mm1 = _mm_insert_pi16(mm1, *((int32_t*)v+pitch), 2);
-    mm1 = _mm_insert_pi16(mm1, *((int32_t*)(v+pitchx3)), 3);
-    __m64 mm2 = mm0;
-    __m64 mm3 = mm1;
+    xmm7 = _mm_cmpeq_epi8(xmm7, xmm7);
 
-    mm7 = _mm_srli_pi16(mm7, 8);
+    xmm0 = _mm_insert_epi16(xmm0, *((int32_t*)v), 2);
+    xmm0 = _mm_insert_epi16(xmm0, *((int32_t*)(v+pitchx2)), 3);
+    xmm1 = _mm_insert_epi16(xmm1, *((int32_t*)v+pitch), 2);
+    xmm1 = _mm_insert_epi16(xmm1, *((int32_t*)(v+pitchx3)), 3);
+    __m128i xmm2 = xmm0;
+    __m128i xmm3 = xmm1;
 
-    mm0 = _mm_and_si64(mm0, mm7);
+    xmm7 = _mm_srli_epi16(xmm7, 8);
 
-    mm1 = _mm_slli_pi16(mm1, 8);
-    mm2 = _mm_srli_pi16(mm2, 8);
+    xmm0 = _mm_and_si128(xmm0, xmm7);
 
-    mm3 = _mm_subs_pu8(mm3, mm7);
+    xmm1 = _mm_slli_epi16(xmm1, 8);
+    xmm2 = _mm_srli_epi16(xmm2, 8);
 
-    mm0 = _mm_or_si64(mm0, mm1);
-    mm2 = _mm_or_si64(mm2, mm3);
+    xmm3 = _mm_subs_epu8(xmm3, xmm7);
 
-    mm0 = _mm_sad_pu8(mm0, mm2);
+    xmm0 = _mm_or_si128(xmm0, xmm1);
+    xmm2 = _mm_or_si128(xmm2, xmm3);
 
-    return (uint32_t)_mm_cvtsi64_si32(mm0);
+    xmm0 = _mm_sad_epu8(xmm0, xmm2);
+
+    return (uint32_t)_mm_cvtsi128_si32(xmm0);
 }
 
 static __forceinline int32_t vertical_diff_yuy2_chroma(const uint8_t *u, const uint8_t *v, int32_t pitch, const uint8_t *noiselevel)
